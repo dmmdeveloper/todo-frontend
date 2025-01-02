9c576e42e40cbb7609f436115973151e5957ec4d
dmmdeveloper <zongdostm@gmail.com>
2025-01-02 11:11:31 -0800

    Login Form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,11 @@ export default function Home() {
 
   return (<>
 
-  <Link to={"/login"} > Go To Login  </Link>
+<div className="h-screen w-full bg-myBlue">
+<h1>Home PAge</h1>
+<Link to={"/login"} >Go to login </Link>
 
+</div>
 
 </>  )
 }
